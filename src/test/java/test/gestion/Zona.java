6211776b6c01f5d1e72201d8ff168c69b7427d06
Camfez92 <62020207+Camfez92@users.noreply.github.com>
2021-11-23 23:13:03 -0500

    Add files via upload

---
+++ src/test/java/test/gestion/Zona.java
@@ -0,0 +1,60 @@
+package gestion;
+
+import zooAnimales.*;
+import java.util.ArrayList;
+
+public class Zona {
+	
+	
+	private String nombre;
+	private Zoologico zoo;
+	private ArrayList<Animal> animales = new ArrayList<Animal>();
+	
+	
+	public Zona() {
+		zoo = new Zoologico();
+		this.zoo.agregarZonas(this);
+		
+	}
+	
+	
+	public Zona(String nombre, Zoologico zoo) {
+		this.nombre = nombre;
+		this.setZoo(zoo);
+		this.zoo.agregarZonas(this);
+	}
+	
+	
+	public void agregarAnimales(Animal animal) {
+		animales.add(animal);
+	}
+	
+	
+	public int cantidadAnimales() {
+		return animales.size();
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Zoologico getZoo() {
+		return zoo;
+	}
+
+	public void setZoo(Zoologico zoo) {
+		this.zoo = zoo;
+	}
+
+	public ArrayList<Animal> getAnimales() {
+		return animales;
+	}
+
+	public void setAnimales(ArrayList<Animal> animales) {
+		this.animales = animales;
+	}
+}
---
+++ src/test/java/test/gestion/Zoologico.java
@@ -0,0 +1,63 @@
+package gestion;
+import java.util.ArrayList;
+
+public class Zoologico {
+	
+	
+	private String nombre;
+	private String ubicacion;
+	private ArrayList<Zona> zonas;
+	
+	
+	
+	
+	public Zoologico() {}
+	
+	
+	public Zoologico (String nombre, String ubicacion) {
+		this.setNombre(nombre);
+		this.setUbicacion(ubicacion);
+	}
+	
+	
+	public int cantidadTotalAnimales() {
+		int numAnimales=0;
+		for(int i=0;i < zonas.size(); i++) {
+			numAnimales += zonas.get(i).cantidadAnimales();
+		}
+	
+		return numAnimales;
+	}
+	
+	
+	public void agregarZonas(Zona zona) {
+		zonas.add(zona);
+	}
+
+	
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getUbicacion() {
+		return ubicacion;
+	}
+
+	public void setUbicacion(String ubicacion) {
+		this.ubicacion = ubicacion;
+	}
+
+	public ArrayList<Zona> getZonas() {
+		return zonas;
+	}
+
+	public void setZonas(ArrayList<Zona> zonas) {
+		this.zonas = zonas;
+	}
+	
+}
