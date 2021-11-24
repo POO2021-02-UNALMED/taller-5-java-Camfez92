ebb99f38147c2dbd963928ee0faf2b4c98388484
Camfez92 <62020207+Camfez92@users.noreply.github.com>
2021-11-23 23:20:20 -0500

    Add files via upload

---
+++ src/main/java/zooAnimales/Pez.java
@@ -0,0 +1,90 @@
+package zooAnimales;
+
+import java.util.ArrayList;
+
+public class Pez extends Animal {
+	
+	
+	private static ArrayList<Pez> listado = new ArrayList<Pez>();
+	public static int salmones;
+	public static int bacalaos;
+	private String colorEscamas;
+	private int cantidadAletas;
+	
+	
+	
+	
+	public Pez() {
+		setTotalAnimales(getTotalAnimales() + 1);
+		listado.add(this);
+	}
+	
+	
+	public Pez(String nombre, int edad, String habitat, String genero, String colorEscamas, int cantidadAletas) {
+		super(nombre, edad, habitat, genero);
+		this.colorEscamas = colorEscamas;
+		this.cantidadAletas = cantidadAletas;
+		setTotalAnimales(getTotalAnimales() + 1);
+		listado.add(this);
+	}
+		
+	
+	@Override
+	public String movimiento() {
+		return "nadar";
+	}
+	
+	static public Pez crearSalmon(String nombre, int edad, String genero) {
+		salmones++;
+		return new Pez(nombre, edad, "oceano",genero,"rojo", 6);
+	}
+	
+	static public Pez crearBacalao(String nombre, int edad, String genero) {
+		bacalaos++;
+		return new Pez(nombre, edad, "oceano",genero,"gris", 6);
+	}
+	
+	static public int cantidadPeces() {
+		return listado.size();
+	}
+	
+	public static ArrayList<Pez> getListado() {
+		return listado;
+	}
+
+	public static void setListado(ArrayList<Pez> listado) {
+		Pez.listado = listado;
+	}
+
+	public static int getSalmones() {
+		return salmones;
+	}
+
+	public static void setSalmones(int salmones) {
+		Pez.salmones = salmones;
+	}
+
+	public static int getBacalaos() {
+		return bacalaos;
+	}
+
+	public static void setBacalaos(int bacalaos) {
+		Pez.bacalaos = bacalaos;
+	}
+
+	public String getColorEscamas() {
+		return colorEscamas;
+	}
+
+	public void setColorEscamas(String colorEscamas) {
+		this.colorEscamas = colorEscamas;
+	}
+
+	public int getCantidadAletas() {
+		return cantidadAletas;
+	}
+
+	public void setCantidadAletas(int cantidadAletas) {
+		this.cantidadAletas = cantidadAletas;
+	}
+}
